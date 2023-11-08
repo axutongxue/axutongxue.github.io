47e8619666b909ad2c2cbe8987b5615b071eb024
阿虚同学 <37839078+axutongxue@users.noreply.github.com>
2023-11-08 17:03:44 +0800

    Add files via upload

--- js/tyytq.js
+++ js/tyytq.js
@@ -7,12 +7,6 @@ var infoList = [
     img: "https://wework.qpic.cn/wwpic/476504_luohQgS3S-a8ysQ_1695131945",
   },
   {
-    title: "微商一手货源网",
-    content: "拒绝中间商赚差价，人人都能低价拿货！",
-    link: "http://100005.aizhy.com \"target=\"_self\"",
-    img: "https://wework.qpic.cn/wwpic/140374_pj6C0xXgQu-0L0z_1685795685",
-  },
-  {
     title: "ChatGPT加强版4.0，已上线！！（安卓+电脑）",
     content: "无需梯子、极速体验、GPT4通道、超强接口、快速稳定！",
     link: "https://wws.lanzoul.com/b0czq1fgb \"target=\"_self\"",
