e0416c96aaff2fcdba90ffb70d0d714d0f6623c8
阿虚同学 <37839078+axutongxue@users.noreply.github.com>
2024-06-03 16:25:43 +0800

    Add files via upload

--- js/loong.js
+++ js/loong.js
@@ -1,16 +1,10 @@
-var code = ` <div id="{{boxId}}" style="top: 0;left: 0;width: 100vw;height: 100vh;background-color: #b2b2b269;position: absolute;z-index: 9999999;display: flex;justify-content: center;align-items: center;"><div style="width:300px;height:150px;background-color:#fff;border-radius:15px;position:relative;box-shadow:rgb(0 0 0/24%)0px 3px 8px;"> <div style="display: flex; align-items: center;"> <img style="display:flex;align-items:center;width:86px;border-radius:10px;margin:10px;" src="{{img}}" alt="" /> <div style="display:flex;flex-direction:column;"> <h2 style="display:flex;align-items:center;font-size:14px;margin:0 0 6 0;color: crimson;">{{title}}</h2> <p style="display:flex;align-items:center;font-size:12px;margin:0 4 0 0;">{{content}}</p> </div> </div> <div style="display: flex;justify-content: center;padding: 0 12px;"> <button id="btnClose" style="padding:6px 24px;background-color:#fff;width:40%;font-size:16px;border-radius:10px;border:1px solid#ccc;">关闭</button> <a style="margin-left:6px;width:100%;color:#fff;background-color:#1866FC;display:flex;justify-content:center;align-items:center;text-decoration:none;border-radius:10px;border:1px solid#ccc;" href="{{path}}">点击查看</a> </div> </div> </div>`;
+var code = ` <div id="{{boxId}}" style="top: 0;left: 0;width: 100vw;height: 100vh;background-color: #b2b2b269;position: absolute;z-index: 9999999;display: flex;justify-content: center;align-items: center;"><div style="width:300px;height:150px;background-color:#fff;border-radius:15px;position:relative;box-shadow:rgb(0 0 0/24%)0px 3px 8px;"> <div style="display: flex; align-items: center;"> <img style="display:flex;align-items:center;width:86px;border-radius:10px;margin:10px;" src="{{img}}" alt="" /> <div style="display:flex;flex-direction:column;"> <h2 style="display:flex;align-items:center;font-size:14px;margin:0 4 6 0;color: crimson;">{{title}}</h2> <p style="display:flex;align-items:center;font-size:12px;margin:0 4 0 0;">{{content}}</p> </div> </div> <div style="display: flex;justify-content: center;padding: 0 12px;"> <button id="btnClose" style="padding:6px 24px;background-color:#fff;width:40%;font-size:16px;border-radius:10px;border:1px solid#ccc;">关闭</button> <a style="margin-left:6px;width:100%;color:#fff;background-color:#1866FC;display:flex;justify-content:center;align-items:center;text-decoration:none;border-radius:10px;border:1px solid#ccc;" href="{{path}}">点击查看</a> </div> </div> </div>`;
 var infoList = [
   {
     title: "ChatGPT加强版4.0，已上线！！（安卓+电脑）",
     content: "无需梯子、极速体验、GPT4通道、超强接口、快速稳定！",
     link: "https://wws.lanzoul.com/b0czq1fgb \"target=\"_self\"",
     img: "https://wework.qpic.cn/wwpic/124822_T2o42DSBTqOYCNB_1686537148",
-  },
-  {
-    title: "ChatGPT4.0 + Midjourney　国内站-IMYAI智能助手！",
-    content: "站长力荐！人工智能聊天绘画机器人，支持国内外10+款大模型！你身边同学都在用的超级生产力工具！",
-    link: "https://link3.cc/imyai?channel=6 \"target=\"_self\"",
-    img: "https://wework.qpic.cn/wwpic3az/724900_hn9UJh0xS1ysGUt_1714390616",
   },
   {
     title: "【莆田珂珂得物鞋厂】",
@@ -20,7 +14,7 @@ var infoList = [
   },
   {
     title: "阿虚的店铺丨运营商直营授权",
-    content: "低至19/月188G流量！电信、联通、移动套餐任选，可打电话发短信，免费包邮放心选购！",
+    content: "低至19/月188G流量！电信、联通、移动、广电套餐任选，可打电话发短信，免费包邮放心选购！",
     link: "https://axu.simhaoka.com/phone/index?id=B956B2050EAA74084CC3FE6EF37C57CF \"target=\"_self\"",
     img: "https://wework.qpic.cn/wwpic3az/809505_IM7dj_IjRTOVqft_1712198786/0",
   },
@@ -28,7 +22,7 @@ var infoList = [
 
 function setCookie() {
   var date = new Date();
-  var Hour = 5; //设置每小时过期时间
+  var Hour = 6; //设置每小时过期时间
   let expireTime = Hour * 3600 * 1000;
   let expires = date.getTime() + expireTime;
   date.setTime(expires);
